7af073e558df394de1cbd0fc28d61ea30730a0b8
Devin Riegle <riegledevin@gmail.com>
2020-10-13 22:13:19 -0400

    Add some more structure

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ You can view it [here](https://github.com/imartingraham/csv-split).
 I noticed that it was quick slow when processing large files, so I wanted to try
 writing a similar program in Rust to compare the time difference.
 
-| Language | File Size | Line Count | Batch Size | Time in seconds |
-| Rust | | 5 | 1 | |
-| Ruby | | 5 | 1 | |
-| Rust | | 1000 | 1 | |
-| Ruby | | 1000 | 1 | |
-| Rust |  289MB | 10,790,000 | 10,000 | 1.09 |
+| Language | File Size | Line Count | Batch Size | Time |
+| Rust | 4.0K | 5 | 1 | 6.79 micro seconds |
+| Ruby | 4.0K | 5 | 1 | |
+| Rust | 28K | 1000 | 1 | 122.15 micro seconds |
+| Ruby | 28K | 1000 | 1 | |
+| Rust |  289MB | 10,790,000 | 10,000 | 1.05s |
 | Ruby | 289MB | 10,790,000 | 10,000 | |
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use std::fs::File;
 use std::io::{self,BufRead};
+use std::env;
+use std::process;
 
 const BATCH_SIZE: i32 = 10_000;
 
 fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
     let mut header: std::option::Option<String> = None;
-    let file = File::open("./test.csv")?;
+    let file = File::open(config.filename)?;
     let reader = io::BufReader::new(file);
-    let mut batches: std::vec::Vec<std::vec::Vec<String>> = Vec::new();
+    let mut batches: Vec<Vec<String>> = Vec::new();
     let mut current_line: i32 = 0;
 
     let start = std::time::Instant::now();
@@ -15,19 +24,21 @@ fn main() -> std::io::Result<()> {
     for line in reader.lines() {
         current_line += 1;
 
+        let contents = line.expect("Something went wrong while parsing the file");
+
         if header.is_none() {
-            header = Some(line.unwrap());
+            header = Some(contents);
         } else {
             let batch_number = (current_line / BATCH_SIZE) as usize;
 
             match batches.get_mut(batch_number) {
                 None => {
                     let mut new_batch = Vec::new();
-                    new_batch.push(line?);
+                    new_batch.push(contents);
                     batches.insert(batch_number, new_batch);
                 },
                 Some(batch) => {
-                    batch.push(line?);
+                    batch.push(contents);
                 }
             }
         }
@@ -42,7 +53,23 @@ fn main() -> std::io::Result<()> {
         }
     }
 
-    println!("{:.2?} seconds for processing {} batches", elapsed, batches.len());
+    println!("{:.2?} for processing {} batches", elapsed, batches.len());
 
     Ok(())
+}
+
+struct Config {
+    filename: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments");
+        }
+
+        let filename = args[1].clone();
+
+        Ok(Config { filename })
+    }
 }
