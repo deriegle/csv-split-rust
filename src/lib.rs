9c34633556294baaeae97c8c3f3038324d209a7b
Devin Riegle <riegledevin@gmail.com>
2020-10-13 22:53:44 -0400

    Improve code using Iterators

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self,BufRead,Lines};
 use std::error::Error;
+use std::env;
 
 const BATCH_SIZE: i32 = 10_000;
 
@@ -9,12 +10,14 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 2 {
-            return Err("Not enough arguments");
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        // skip first arg which has name of file we're running
+        args.next();
 
-        let filename = args[1].clone();
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't receive a filename.")
+        };
 
         Ok(Config { filename })
     }
@@ -39,40 +42,34 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn process_lines(lines: Lines<io::BufReader<File>>) -> Result<Vec<Vec<String>>, Box<dyn Error>> {
+fn process_lines(mut lines: Lines<io::BufReader<File>>) -> Result<Vec<Vec<String>>, Box<dyn Error>> {
     let mut current_line: i32 = 0;
-    let mut header: std::option::Option<String> = None;
     let mut batches: Vec<Vec<String>> = Vec::new();
+
+    let header = match lines.next().expect("Something went wrong while parsing the file.") {
+        Err(e) => return Err(Box::new(e)),
+        Ok(s) => s,
+    };
 
     for line in lines {
         current_line += 1;
 
         let contents = line.expect("Something went wrong while parsing the file");
+        let batch_number = (current_line / BATCH_SIZE) as usize;
 
-        if header.is_none() {
-            header = Some(contents);
-        } else {
-            let batch_number = (current_line / BATCH_SIZE) as usize;
-
-            match batches.get_mut(batch_number) {
-                None => {
-                    let mut new_batch = Vec::new();
-                    new_batch.push(contents);
-                    batches.insert(batch_number, new_batch);
-                },
-                Some(batch) => {
-                    batch.push(contents);
-                }
+        match batches.get_mut(batch_number) {
+            None => {
+                let mut new_batch = Vec::new();
+                new_batch.push(contents);
+                batches.insert(batch_number, new_batch);
+            },
+            Some(batch) => {
+                batch.push(contents);
             }
         }
     }
 
-    match header {
-        None => println!("File not found."),
-        Some(header_text) => {
-            println!("Header Text: {}", header_text);
-        }
-    }
+    println!("Header Text: {}", header);
 
     return Ok(batches);
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use std::process;
 use csvsplit::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
